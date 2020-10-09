8688fa29b2a07aec8a5d2283af63088972cde098
nguyentan1510 <nguyentan15102000@gmail.com>
2020-10-09 18:39:59 +0700

    Updates

---
+++ precache-manifest.198c6a34a2870cc6bee0fe87bb9aefb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee29c8f6eadcbaa279941019063ef61d",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "4af599c76f8fbbc345f2",
+    "url": "/profile/static/css/main.20fb129c.chunk.css"
+  },
+  {
+    "revision": "6de2caf1eebd115590bf",
+    "url": "/profile/static/js/2.bf482b9d.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/profile/static/js/2.bf482b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af599c76f8fbbc345f2",
+    "url": "/profile/static/js/main.1fd5ced2.chunk.js"
+  },
+  {
+    "revision": "ef72a30605d185504f73",
+    "url": "/profile/static/js/runtime-main.87e3449c.js"
+  }
+]);
